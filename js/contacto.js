44ed4c244c941aef1b0fc2119e1b3907ccc392ed
Yahir Klein Quintero Suarez <yahir.kleins@gmail.com>
2025-04-16 22:09:04 -0500

    add pages

--- js/contacto.js
+++ js/contacto.js
@@ -1,12 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-
-    if (menuToggle && navMenu) {
-        menuToggle.addEventListener('click', function() {
-            navMenu.classList.toggle('active');
-        });
-    }
 
     const whatsappButton = document.getElementById('send-whatsapp');
     const form = document.getElementById('whatsapp-form');
--- js/index.js
+++ js/index.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('.nav-menu');
-
     if (menuToggle && navMenu) {
         menuToggle.addEventListener('click', function() {
             navMenu.classList.toggle('active');
         });
 
-        // Opcional: Cerrar menú si se hace clic en un enlace (útil en Single Page App, no tanto aquí)
-        /*
+        
         const navLinks = navMenu.querySelectorAll('a');
         navLinks.forEach(link => {
             link.addEventListener('click', () => {
@@ -17,6 +15,6 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         });
-        */
+        
     }
 });
